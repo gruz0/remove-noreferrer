9c4bace98c10a7989ea821970f962bd4ddfd0af6
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-03-21 11:50:19 +0300

    Copy banners and icons to svn directory

--- bin/release.sh
+++ bin/release.sh
@@ -5,4 +5,6 @@ rm -rf svn
 svn co https://plugins.svn.wordpress.org/remove-noreferrer/ svn
 
 cp -Rv assets/screenshots/* svn/assets/
+cp -Rv assets/banners/* svn/assets/
+cp -Rv assets/icons/* svn/assets/
 cp -Rv readme.txt remove-noreferrer.php svn/trunk/
